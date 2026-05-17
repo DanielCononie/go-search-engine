a94344f7e9ef5a4ae4e6cd2b239a5a6247eeda96
Daniel Cononie <danielcononie1278@gmail.com>
2026-05-16 23:36:25 -0400

    push

--- internal/crawler/fetcher.go
+++ internal/crawler/fetcher.go
@@ -1,6 +1,8 @@
 package crawler
 
 import (
+	"fmt"
+
 	"github.com/DanielCononie/go-search-engine.git/go-search-engine/internal/models"
 )
 
@@ -18,5 +20,6 @@ func FetchURL(url string) (models.FetchResult, error) {
 }
 
 func FetchURLs(urls []string) []models.FetchResult {
+	fmt.Print(urls)
 	return []models.FetchResult{}
 }
